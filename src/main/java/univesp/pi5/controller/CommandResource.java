22b978e1e285607a947fc6b8265cebc7140b778f
fernando-utiyama <fernandoutiyama@gmail.com>
2021-06-13 18:56:54 -0300

    Ajuste delay

--- src/main/java/univesp/pi5/controller/CommandResource.java
+++ src/main/java/univesp/pi5/controller/CommandResource.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@CrossOrigin(origins = "http://univesp-campinas-pi5-ui.herokuapp.com")
 @RestController
 @RequestMapping("/api")
 @Slf4j
